b7fa81157327ce793b8764a8dc361ba6e629d744
Vova <tarchavovaswat@gmail.com>
2015-06-08 11:36:50 +0300

    add columns to LocationEntry and delete /app/ from address in import  WeatherDbHelper and Tests

--- app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -40,6 +40,13 @@ public class WeatherContract {
      */
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // In order to uniquely pinpoint the location on the map when we launch the
+        // map intent, we store the latitude and longitude as returned by openweathermap.
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
 
     }
 
