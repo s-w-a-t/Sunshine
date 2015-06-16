16e1c25ff847943adf2896c6e6592e39871ecc83
Vova <tarchavovaswat@gmail.com>
2015-06-16 12:29:06 +0300

    change interface... add list_item_forecast_today and add some view to list_item_forecast

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -6,14 +6,9 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.android.sunshine.data.WeatherContract;
 
-/**
- * {@link ForecastAdapter} exposes a list of weather forecasts
- * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
- */
 public class ForecastAdapter extends CursorAdapter {
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
@@ -61,7 +56,7 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+//        TextView tv = (TextView)view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
